ab3ab8d2df9ad4cd4e39359658952e8a76eea469
Jean <jhautier@hotmail.fr>
2022-04-20 22:23:41 +0200

    fix modification of AppState

--- src/Nested1.js
+++ src/Nested1.js
@@ -1,21 +1,24 @@
 import { useState } from "react";
 
-const Nested1 = ({nested1Props})=>{
-  const [nestedState, setNestedState] = useState('nestedState')
+const Nested1 = ({ nested1Props }) => {
+  const [nestedState, setNestedState] = useState("nestedState");
 
-const handleClick = () => {
-  nested1Props = Math.random();
-  console.log(nested1Props)
+  const handleClick = () => {
+    nested1Props = Math.random();
+    console.log(nested1Props);
+  };
+  nested1Props = "changedProps";
+  console.log("Nested1 has rendered");
 
-}
-nested1Props = "changedProps"
-console.log("Nested1 has rendered")
-
-return <div className="nested1">App Props: {nested1Props}
-nested State: {nestedState}
-<button onClick={handleClick}>change nested1Props from Nested1</button>
-<button onClick={()=>setNestedState(Math.random())}> change nestedState from Nested1</button>
-</div>
-  
-}
-export default Nested1
+  return (
+    <div className="nested1">
+      App Props: {nested1Props}
+      nested State: {nestedState}
+      <button onClick={handleClick}>change nested1Props from Nested1</button>
+      <button onClick={() => setNestedState(Math.random())}>
+        change nestedState from Nested1
+      </button>
+    </div>
+  );
+};
+export default Nested1;
--- src/Nested2.js
+++ src/Nested2.js
@@ -1,11 +1,14 @@
-const Nested2 = ({appState, setAppState})=>{
+const Nested2 = ({ appState, setAppState }) => {
   const handleClick = () => {
-    setAppState(Math.random())
-  }
-  console.log("Nested2 has rendered")
+    setAppState(Math.random());
+  };
+  console.log("Nested2 has rendered");
 
-return <div className="nested2"> appState: {appState} 
-<button onClick={handleClick}> change state </button>
-</div>
-}
-export default Nested2
+  return (
+    <div className="nested2">
+      appState: {appState}
+      <button onClick={handleClick}> change App state </button>
+    </div>
+  );
+};
+export default Nested2;
